aa06c88ead8431435ee050bd976a35440b541a28
josam <josam.developer@gmail.com>
2025-03-24 19:50:19 +0100

    Improvements and fixes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "942974020"
+    serviceWorkerVersion: "3943152083"
   }
 });
